0ab13ef6aeed6c394c9eb4a430c297a90d12b631
biswajitbhunia <44657622+biswajitbhunia@users.noreply.github.com>
2024-02-06 07:45:48 +0530

    Create pattern_15_c.java

---
+++ pattern_15_c.java
@@ -0,0 +1,10 @@
+class pattern_15_c {
+    public static void main(String arg[]) {
+        int i, j;
+        i = 5;
+        for (j = 1; j <= i; j++) {
+            System.out.print("hi");
+        }
+
+    }
+}
