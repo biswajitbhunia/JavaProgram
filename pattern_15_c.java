0c74e1711e9c6d3210f32615cc9c525a6e8fb6e5
biswajitbhunia <44657622+biswajitbhunia@users.noreply.github.com>
2024-02-22 20:01:31 +0530

    Update pattern_15_c.java

--- pattern_15_c.java
+++ pattern_15_c.java
@@ -1,11 +1,13 @@
 class pattern_15_c {
     public static void main(String arg[]) {
 
-      for(int i = 9;i>=1;i-=2){
-        for (int j = 1; j <=5; j++) {
-            System.out.print(i + " ");
-         
-        }
+     int k=1;
+        for(int i = 1;i<=5;i++){
+        for (int j = 1; j <=k; j++) {
+            System.out.print("*" + " ");
+            
+          }
+          k=k+2;
         System.out.println("");
         }
           
